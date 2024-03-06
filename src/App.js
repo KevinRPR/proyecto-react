36404817d132167b4f141c5e1e5e6babe862a4e6
Kevin Palencia <kevinpalencia@protonmail.com>
2024-03-06 11:21:49 -0400

    agregue varias cosas, varios archivos, espero que sirva de algo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+function HolaMundo(){
+  return <h1>Hola mundo</h1>
+}
+
 function App() {
-  
+  return <HolaMundo />;
 }
 
 export default App;
